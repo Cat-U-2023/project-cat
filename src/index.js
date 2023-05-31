97754151adf0ded23796c2ca785b3f43af291fa8
Dave Alvarez <alvarezruizdavidJ@gmail.com>
2023-05-31 11:43:40 +0100

    add new section About-Us
    img of the beautiful team

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-$(window).scroll(function(){ 
-    if ($(this).scrollTop() > 1000) { 
-        $('.back-to-top').fadeIn(); 
-    } else { 
-        $('.back-to-top').fadeOut(); 
-    } 
+// const showOnPx = 100;
+// const backToTopButton = document.querySelector(".back-to-top");
+
+// const scrollContainer = () => {
+//   return document.documentElement || document.body;
+// };
+
+// document.addEventListener("scroll", () => {
+//   if (scrollContainer().scrollTop > showOnPx) {
+//     backToTopButton.classList.remove("hidden");
+//   } else {
+//     backToTopButton.classList.add("hidden");
+//   }
+// });
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 500) {
+    $(".back-to-top").fadeIn();
+  } else {
+    $(".back-to-top").fadeOut();
+  }
 });
 
-$('.back-to-top').click(function(){ 
-    $("html, body").animate({ scrollTop: 0 }, 600); 
-    return false; 
+$(".back-to-top").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, 10);
+  return false;
 });
