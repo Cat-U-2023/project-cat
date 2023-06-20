4bce7bc1dbddccbb3cf4e737affc9438fe6e55a7
Nirwita <acasdigitalwork@gmail.com>
2023-06-20 12:53:40 +0000

    vite config changed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import path from "path";
-const isGitHubPages = true;
+const isGitHubPages = false;
 const folderName = path.basename(process.cwd()) + "/";
-const mode = process.env.NODE_ENV === "production" ? "production" : "development";
+const mode =
+  process.env.NODE_ENV === "production" ? "production" : "development";
 const base = mode === "production" && isGitHubPages ? "/" + folderName : "/";
 
 export default {
@@ -11,6 +12,6 @@ export default {
   publicDir: "../public",
   build: {
     outDir: "../dist",
-    assetsDir: "./"
-  }
+    assetsDir: "./",
+  },
 };
